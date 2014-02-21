592ba5f579f05dfc27003bc692c19f918c80394f
Alexis Schad <schadoc_alex@hotmail.fr>
2014-02-21 22:26:18 +0100

    Passage à CocoonJS et CreateJS (intro)

--- src/intro/intro.js
+++ src/intro/intro.js
@@ -20,6 +20,8 @@ Intro.logo = function() {
 	Destroy.all();
 	logo = new Logo();
 	logo.display();
+	gui.menuButton();
+	// initConstantes();
 	
 	Effects.respire('logo', logo.getNode(), 0.2, 1);
 	Event.onTap('logo', logo, function() {
@@ -34,40 +36,39 @@ Intro.laSeparation = function() {
 	Destroy.all();
 	
 	var anim_duration = 2000;
-	
+	var zoom = 2;
 	la_separation = new Word('Separation', 'Perception', 5);
-	la_separation.setZoom(2);
+	la_separation.setZoom(zoom);
 	la_separation.setCenterXY(W / 2, H / 2);
-	// la_separation.getNode().alpha = 0;
+	la_separation.getNode().alpha = 0;
 	la_separation.display();
+	la_separation.addGesture();
 	
-	// Tween.get(la_separation.getNode())
-		// .to({
-			// alpha: 1,
-			// }, anim_duration/2, Ease.sineOut)
-		// .call();
+	Tween.get(la_separation.getNode())
+		.to({alpha:1}, anim_duration, Ease.sineIn);
+
 	cutLaSeparation();
 	
 	function cutLaSeparation() {
-		la_separation.addGesture();
 		Effects.respire('la_separation', la_separation.getNode(), 0.5, 1);
 		la_separation.setDone('animate', function() {
 			Effects.stopRespire('la_separation');
 		});
 		la_separation.setDone('animationFinished', function() {
+			la_separation.removeGesture();
 			setTimeout(function() {
 				Tween.get(la_separation.getNodeUp())
 					.to({
-							y: H / 4,
+							y: H / (2 * fontConst.car.scale * zoom),
 							alpha: 0,
 						}, anim_duration, Ease.sineIn);
 				
 				Tween.get(la_separation.getNodeDown())
 					.to({
-							y: -H / 4,
+							y: -H / (2 * fontConst.car.scale * zoom),
 							alpha: 0,
-						}, anim_duration, Ease.sineIn);
-					// .call(Menu.start);
+						}, anim_duration, Ease.sineIn)
+					.call(Menu.start);
 			}, 500);
 		});
 	}
