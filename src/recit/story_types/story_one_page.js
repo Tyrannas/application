650c3d86993c928a78218092a6dd681510d68257
Tytan <quentin.chateau@gmail.com>
2014-02-28 21:28:49 +0100

    ajout de stories

--- src/recit/story_types/story_one_page.js
+++ src/recit/story_types/story_one_page.js
@@ -15,7 +15,7 @@ function StoryOnePage(json_def) {
 		this.name = json_def.name;
 	}
 	this.pages[0].setYsize(H);
-	this.pages[0].setCenterXY(W/2,H/3);
+	this.pages[0].setCenterXY(W/2,H/2);
 	this.generate();
 }
 StoryOnePage.prototype = new Story(); //Heritage partie 2
