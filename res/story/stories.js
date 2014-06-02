a5201c007c9355ac761c60318832e505005ba18e
Tytan <quentin.chateau@gmail.com>
2014-06-02 18:41:29 +0200

    debut stories en

--- res/story/stories.js
+++ res/story/stories.js
@@ -7,6 +7,9 @@ StoriesDb.fr_stories = [
 	];
 
 StoriesDb.en_stories = [
+/*
+	'{"isStatic":true, "name":"Separation", "type":"one_page", "pages":[{"lines":[{"words":[{"value":"this"}, {"value":"separation", "next_value":"perception", "police":?, "code":"??", "zoom":1}]}, {"value":"which crept in the"}, {"words":[{"value":"shading", "next_value":"slicing", "police":?, "code":"??", "zoom":1}, {"value":"of words"}]}, {"words":[{"value":"shreds", "next_value":"chases", "police":?, "code":"??", "zoom":1}]}]}]}'
+	*/
 	'{"isStatic":true, "name":"EN Demi tour", "type":"one_page", "pages":[{"lines":[{"words":[{"value":"Demi tour", "zoom":1}]}, {"words":[{"value":"marche", "next_value":"arriere", "police":1, "code":"IIIIIIILIIL", "zoom":2}]}, {"words":[{"value":" "}]}]}]}',
 	'{"isStatic":true, "name":"EN Separation", "type":"one_page", "pages":[{"lines":[{"words":[{"value":"CETTE", "zoom":1}, {"value":"SEPARATION", "next_value":"PERCEPTION", "police":0, "code":"AAAAAATIAII", "zoom":1}]}, {"words":[{"value":"OMBRE QUI GLISSE"}]}, {"words":[{"value":"SABRE", "next_value":"SACRE", "police":2, "zoom":1}, {"value":"NOTRE UNION", "police":2}]}]}]}',
 	'{"isStatic":true, "name":"EN Quotidien", "type":"one_page", "pages":[{"lines":[{"words":[{"value":"BOUCHER", "next_value":"BOXEUSE", "police":0, "code":"AAIICIIAA", "zoom":1}]}, {"words":[{"value":"LUC", "next_value":"LOU", "police":1, "zoom":1}, {"value":"DECOUPE", "next_value":"DEGUSTE", "police":1, "zoom":1}, {"value":"DU"}, {"value":"PORC", "next_value":"SANG", "police":0, "code":"AAAA", "zoom":1}]}]}]}',
