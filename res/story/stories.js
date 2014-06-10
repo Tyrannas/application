53d69ee0baaa399fee0fe9084d2a04e329b09c61
Alexis Schad <schadoc_alex@hotmail.fr>
2014-06-10 09:15:53 +0200

    Correction bug y avec le code "i" en demibas_min

--- res/story/stories.js
+++ res/story/stories.js
@@ -7,9 +7,7 @@ StoriesDb.fr_stories = [
 	];
 
 StoriesDb.en_stories = [
-	'{"isStatic":true, "name":"EN Demi tour", "type":"one_page", "pages":[{"lines":[{"words":[{"value":"Demi tour", "zoom":1}]}, {"words":[{"value":"marche", "next_value":"arriere", "police":1, "code":"IIIIIIILIIL", "zoom":2}]}, {"words":[{"value":" "}]}]}]}',
-	'{"isStatic":true, "name":"EN Separation", "type":"one_page", "pages":[{"lines":[{"words":[{"value":"CETTE", "zoom":1}, {"value":"SEPARATION", "next_value":"PERCEPTION", "police":0, "code":"AAAAAATIAII", "zoom":1}]}, {"words":[{"value":"OMBRE QUI GLISSE"}]}, {"words":[{"value":"SABRE", "next_value":"SACRE", "police":2, "zoom":1}, {"value":"NOTRE UNION", "police":2}]}]}]}',
-	'{"isStatic":true, "name":"EN Quotidien", "type":"one_page", "pages":[{"lines":[{"words":[{"value":"BOUCHER", "next_value":"BOXEUSE", "police":0, "code":"AAIICIIAA", "zoom":1}]}, {"words":[{"value":"LUC", "next_value":"LOU", "police":1, "zoom":1}, {"value":"DECOUPE", "next_value":"DEGUSTE", "police":1, "zoom":1}, {"value":"DU"}, {"value":"PORC", "next_value":"SANG", "police":0, "code":"AAAA", "zoom":1}]}]}]}',
+	'{"isStatic":true, "name":"Separation", "type":"one_page", "pages":[{"lines":[{"words":[{"value":"THIS", "zoom":1}, {"value":"SEPARATION", "next_value":"PERCEPTION", "police":0, "code":"AAAAAATIAII", "zoom":1}]}, {"words":[{"value":"WHICH CREPT"}]}, {"words":[{"value":"IN THE SHADING OF WORDS"}]}, {"words":[{"value":"SHREDS", "next_value":"CHASES", "police":2, "code":"BHABBB", "zoom":1}, {"value":"ME", "police":2}]}]}]}',
 	];
 
 
