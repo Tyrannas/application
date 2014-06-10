83c9789687bc78b4985ad687b054671908d32ec8
Alexis Schad <schadoc_alex@hotmail.fr>
2014-06-10 11:30:07 +0200

    Ajout de la police de l'ombre dans les exemples du salon

--- src/lib_separation/word_constructor/ombre.js
+++ src/lib_separation/word_constructor/ombre.js
@@ -1,60 +1,43 @@
 /*** TEMP ***/
 
 function Word_ombre(data) {
-	/*
-	this.container = new Kinetic.Group({
-		width: 771/4,
-		height: 267/4,
-		y: data.cst.police[data.police].offset,
-	});
+	opacity_OMBRE = 1;
+	if(Menu.language == 'fr') {
+		this.CYGNE = new createjs.Bitmap(res('CYGNE'));
+		this.OMBRE = new createjs.Bitmap(res('OMBRE'));
+		var scale = 1.2;
+	} else {
+		this.CYGNE = new createjs.Bitmap(res('SLICING'));
+		this.OMBRE = new createjs.Bitmap(res('SHADING'));
+		var scale = 1;
+	}
+	var offsetX = 40;
+	var offsetY = -20;
+	this.OMBRE.scaleX = scale;
+	this.OMBRE.scaleY = scale;
+	this.CYGNE.scaleX = scale;
+	this.CYGNE.scaleY = scale;
+	this.OMBRE.x = offsetX;
+	this.CYGNE.x = offsetX;
+	this.OMBRE.y = offsetY;
+	this.CYGNE.y = offsetY;
+	this.CYGNE.alpha = 0;
 
-	this.container.add(CYGNE);
-	this.container.add(OMBRE);
+	this.container = new createjs.Container();
+	this.container.width = this.OMBRE.image.width * scale;
+	this.container.height = this.OMBRE.image.height * scale;
 	
-	this.destroy = function() {
-		// this.container.destroy();
+	this.container.addChild(this.CYGNE, this.OMBRE);
+	
+	/*this.destroy = function() {
+		this.CYGNE.destroy();
+		this.OMBRE.destroy();
 	}*/
 }
-/*
-var imgs = new Array();
-imgs[0] = new Image();
-imgs[1] = new Image();
 
-var CYGNE = null;
-var OMBRE = null;
-
-if(appOnDevice()) {
-	var path = location.pathname;
-	var tab = path.split("/");
-	imgs[0].src = path.replace(tab[tab.length-1], "imgs/aide/OMBRE.jpg");
-	imgs[1].src = path.replace(tab[tab.length-1], "imgs/aide/CYGNE.jpg");
-}
-else {
-	imgs[0].src = "imgs/aide/OMBRE.png";
-	imgs[1].src = "imgs/aide/CYGNE.png";
+Word_ombre.prototype.destroy = function() {
+	this.container.removeChild(this.CYGNE, this.OMBRE);
+	stage.removeChild(this.container);
 }
 
-this.container = new Kinetic.Group({
-	width: 771,
-	height: 267,
-});
-var container_kinetic = this.container;
-
-imgs[0].onload = function() {
-	OMBRE = new Kinetic.Image({
-		image: imgs[0],
-		width: 771/4,
-		height: 267/4,
-		y: -12,
-	});
-}
-imgs[1].onload = function() {
-	CYGNE = new Kinetic.Image({
-		image: imgs[1],
-		width: 771/4,
-		height: 267/4,
-		y: -12,
-	});
-}
-*/
 scriptLoaded('src/lib_separation/word_constructor/ombre.js');
